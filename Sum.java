15777458e93b3441ed9f855d493863d74a5ec5a9
Dan Kim <jidankim@gmail.com>
2016-02-10 10:47:10 -0500

    sum

--- Sum.java
+++ Sum.java
@@ -11,7 +11,7 @@ public class Sum {
     // Iterative version: uses a loop
     public static int sumI(int n) {
 	int ans = 0;
-	for (int i = 1; i < n; i++) {
+	for (int i = 1; i < n+1; i++) {
 	    ans += i;
 	}
 	return ans;
@@ -23,10 +23,37 @@ public class Sum {
 	return n + sumR(n-1); 
     }
 
+    // task 2
+    // ex. sum(2,3,5) = 2 + 5 + 8 + 11 + 14 = 40
+   
+    // pre: a,d,n are positive integers
+    // post: a + (a + d) + (a + 2d) + ... + (a + (n-1)d)
+
+    // explicit
+    public static int sum(int a, int d, int n) {
+	return (2*a + (n-1)*d) * n / 2;
+    }
+
+    public static int sumI(int a, int d, int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+	    sum += a + i*d;
+	}
+	return sum;
+    }
+
+    public static int sumR(int a, int d, int n) {
+	if (n == 1) return a;
+	return a + sumR(a+d, d, n-1);
+    }
+
     public static void main(String[] args) {
 	int N = Integer.parseInt(args[0]);
 	System.out.println("Sum : " + sum(N));
 	System.out.println("SumI : " + sumI(N));
 	System.out.println("SumR : " + sumR(N));
+	System.out.println("sum : " + sum(2,3,N));
+	System.out.println("sumI : " + sumI(2,3,N));
+	System.out.println("sumR : " + sumR(2,3,N));
     }
 }

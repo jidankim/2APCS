1374363c7c89816bcf85509b9aed61ddbd41c010
Dan Kim <jidankim@gmail.com>
2016-02-23 11:11:36 -0500

    linkedlist

--- SLinkedList.java
+++ SLinkedList.java
@@ -21,8 +21,40 @@ public class SLinkedList {
     }
 
     public void addLast(String value) {
-	_tail.setNext(new Node(value, null));
+	Node newLast = new Node(value, null);
+	if (_size == 0) _head = _tail = newLast;
+	else {
+	    _tail.setNext(newLast);
+	    _tail = newLast;
+	}
 	_size++;
+    }
+
+    public String removeFirst() {
+	if (_size == 0) throw new IllegalStateException();
+	String ans = _head.getValue();
+	if (_size == 1) _tail = null;
+        _head = _head.getNext();
+	_size--;
+	return ans;
+    }
+
+    public String removeLast() {
+	String ans = _tail.getValue();
+	if (_size <= 1) { 
+	    removeFirst();
+	} else {
+	    Node n = _head;
+	    while (n.getNext() != _tail) n = n.getNext();
+	    n.setNext(null);
+	    _tail = n;
+	    _size--;
+	}
+	return ans;
+    }
+
+    public int size() {
+	return _size;
     }
 
     public String toString() {
@@ -44,6 +76,8 @@ public class SLinkedList {
 	System.out.println(L); // Mary Bill
 	L.addFirst("Sue");
 	System.out.println(L); // Sue Mary Bill
+	L.removeFirst();
+	System.out.println(L);
     }
 
 }

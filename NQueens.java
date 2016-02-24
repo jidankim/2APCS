bd10b19c696090039833930a9624c6831e0b1360
Dan Kim <jidankim@gmail.com>
2016-02-24 05:36:22 -0500

    NQueens

--- NQueens.java
+++ NQueens.java
@@ -20,12 +20,9 @@ public class NQueens{
     // Calls solve(int,int).
     public void solve(){
 	int N = _board.length;
-	for (int i = 0; i <= N/2; i++)
-	    for (int j = 0; j <= N/2; j++)
-		if (solve(i,j)) {
-		    return;
-		}
-		
+        for (int i = 0; i < N; i++)
+            if (solve(i,0)) 
+                return;
     }
 
     // pre: _n < N*N
@@ -33,44 +30,51 @@ public class NQueens{
     //                false otherwise
     private boolean solve(int x, int y){
 	try {
-	    Thread.sleep(1);                 //1000 milliseconds is one second.
+	    Thread.sleep(10);                 //1000 milliseconds is one second.
 	} catch(InterruptedException ex) {
 	    Thread.currentThread().interrupt();
 	}
 	// Your code goes here.
 	int N = _board.length;
-	//check if out of bounds
-	if (x < 0 || y < 0) return false;
-	if (x >= N || y >= N) return false;
-	//check if position is occupied
+        //check if position is occupied
 	if (_board[x][y] != 0) return false;
 	//occupy position
-	_board[x][y] = ++_n;
-	//is it solved?
-	if (_n == N*N) return true;
+	_board[x][y] = 2*N - 1;
+        _n++;
+        //mark all spaces protected by this queen
+        markAll(x,y,true);
+        //is it solved?
+	if (_n == N) return true;
 	System.out.println(this);
 	//if not solved, continue down each path
-	if (solve(x-2, y-1)) return true;
-	if (solve(x-2, y+1)) return true;
-	if (solve(x-1, y+2)) return true;
-	if (solve(x+1, y+2)) return true;
-	if (solve(x+2, y+1)) return true;
-	if (solve(x+2, y-1)) return true;
-	if (solve(x+1, y-2)) return true;
-	if (solve(x-1, y-2)) return true;
-
+        for (int i = 0; i < N; i++)
+            if (_board[i][_n] == 0)
+                if (solve(i, _n)) return true;
 	//No solution found, back track
+        markAll(x,y,false);
 	_board[x][y] = 0;
 	System.out.println(this);
 	_n--;
 	return false;
     }
+    
+    private void markAll(int x, int y, boolean mark) {
+        int N = _board.length;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (i == x || j == y || j-i == y-x || j+i == y+x) {
+                    if (mark) _board[i][j]++;
+                    else _board[i][j]--;
+                }
+    }
 
     public String toString(){
 	String ans = "";
-	for (int i = 0; i < _board.length; i++){
-	    for (int j = 0; j < _board[i].length; j++){
-		ans += _board[i][j] + " ";
+        int N = _board.length;
+	for (int i = 0; i < N; i++){
+	    for (int j = 0; j < N; j++){
+		if (_board[i][j] == 2*N) ans += "Q ";
+                else ans += "- ";
 	    }
 	    ans += "\n";
 	}
